d8cbdee6eedf1052ac21e9e9b64dd328e2e960fc
tungDao <explodingww@gmail.com>
2024-10-13 17:48:59 +0700

    initial task

---
+++ discord_auto.py
@@ -0,0 +1,19 @@
+import os
+import requests
+
+user_token = os.environ["discord_user_token"]
+channel_id = os.environ["discord_id"]
+
+url = f"https://discord.com/api/v9/channels/{channel_id}/messages"
+
+# Set up the request headers
+headers = {
+    "Authorization": user_token,
+    "Content-Type": "application/json"  # Set content type to JSON
+}
+
+payload = {
+    "content": "text form GHA of repo: https://github.com/tungdaoxuan123/GHA_for_discord_bot"
+}
+
+response = requests.post(url, json=payload, headers=headers)
