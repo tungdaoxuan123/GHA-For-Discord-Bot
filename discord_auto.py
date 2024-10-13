258e0ff5cb904377a989cccd3dfe3b154ed8c8e1
tungDao <explodingww@gmail.com>
2024-10-13 18:12:25 +0700

    test roll and get kakera

--- discord_auto.py
+++ discord_auto.py
@@ -1,4 +1,6 @@
 import os
+import json
+import re
 import requests
 
 user_token = os.environ["DISCORD_USER_TOKEN"]
@@ -12,8 +14,31 @@ headers = {
     "Content-Type": "application/json"  # Set content type to JSON
 }
 
-payload = {
-    "content": "text form tungdao GHA"
-}
+def fetch_last_message():
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        messages = response.json()
+        data = messages[0] if messages else {}
+        if data:
+            description = data['embeds'][0]['description']
+            match = re.search(r'\*\*(\d+)\*\*', description)
 
-response = requests.post(url, json=payload, headers=headers)
+            if match:
+                kakera = match.group(1)
+                return kakera
+            else:
+                print("No number found.")
+                return 0
+    
+                
+
+def send_text(text):
+    payload = {
+        "content": text
+    }
+    response = requests.post(url, json=payload, headers=headers)
+
+send_text("tungdao GHA: start roll")
+send_text("$w")
+kakera = fetch_last_message()
+send_text("Kakera is : {kakera}")
